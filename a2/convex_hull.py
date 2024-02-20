a34be1078022bd117109573aeefcdddf30fd13d4
nrobillard <71105877+nrobillard@users.noreply.github.com>
2024-02-20 14:56:20 -0500

    Update convex_hull.py

--- a2/convex_hull.py
+++ a2/convex_hull.py
@@ -9,6 +9,85 @@ Point = Tuple[int, int]
 
 mid = [0, 0]
 
+def y_intercept(p1: Point, p2: Point, x: int) -> float:
+    """
+    Given two points, p1 and p2, an x coordinate from a vertical line,
+    compute and return the y-intercept of the line segment p1->p2
+    with the vertical line passing through x.
+    """
+    x1, y1 = p1
+    x2, y2 = p2
+    slope = (y2 - y1) / (x2 - x1)
+    return y1 + (x - x1) * slope
+
+
+def triangle_area(a: Point, b: Point, c: Point) -> float:
+    """
+    Given three points a,b,c,
+    computes and returns the area defined by the triangle a,b,c.
+    Note that this area will be negative if a,b,c represents a clockwise sequence,
+    positive if it is counter-clockwise,
+    and zero if the points are collinear.
+    """
+    ax, ay = a
+    bx, by = b
+    cx, cy = c
+    return ((cx - bx) * (by - ay) - (bx - ax) * (cy - by)) / 2
+
+
+def is_clockwise(a: Point, b: Point, c: Point) -> bool:
+    """
+    Given three points a,b,c,
+    returns True if and only if a,b,c represents a clockwise sequence
+    (subject to floating-point precision)
+    """
+    return triangle_area(a, b, c) < -EPSILON
+
+
+def is_counter_clockwise(a: Point, b: Point, c: Point) -> bool:
+    """
+    Given three points a,b,c,
+    returns True if and only if a,b,c represents a counter-clockwise sequence
+    (subject to floating-point precision)
+    """
+    return triangle_area(a, b, c) > EPSILON
+
+def collinear(a: Point, b: Point, c: Point) -> bool:
+    """
+    Given three points a,b,c,
+    returns True if and only if a,b,c are collinear
+    (subject to floating-point precision)
+    """
+    return abs(triangle_area(a, b, c)) <= EPSILON
+
+
+def sort_clockwise(points: List[Point]):
+    """
+    Sorts `points` by ascending clockwise angle from +x about the centroid,
+    breaking ties first by ascending x value and then by ascending y value.
+
+    The order of equal points is not modified
+
+    Note: This function modifies its argument
+    """
+    # Trivial cases don't need sorting, and this dodges divide-by-zero errors
+    if len(points) < 2:
+        return
+
+    # Compute the centroid
+    centroid_x = sum(p[0] for p in points) / len(points)
+    centroid_y = sum(p[1] for p in points) / len(points)
+
+    # Sort by ascending clockwise angle from +x, breaking ties with ^x then ^y
+    def sort_key(point: Point):
+        angle = math.atan2(point[1] - centroid_y, point[0] - centroid_x)
+        normalized_angle = (angle + math.tau) % math.tau
+        return normalized_angle, point[0], point[1]
+
+    # Sort the points
+    points.sort(key=sort_key)
+
+
 # Checks whether the line is crossing the polygon
 def orientation(a, b, c):
     res = (b[1]-a[1]) * (c[0]-b[0]) - (c[1]-b[1]) * (b[0]-a[0])
@@ -95,6 +174,9 @@ def compute_hull(points: List[Point]) -> List[Point]:
 
     middle = len(points) // 2
 
+    # sorts points
+    sort_clockwise(points)
+
     left = points[:middle]
     right = points[middle:]
 
@@ -160,4 +242,3 @@ def merge_convex_hulls(left_hull, right_hull):
     merged_hull.append(upper_b)
 
     return merged_hull
-
